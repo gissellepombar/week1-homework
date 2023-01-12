13a30275c6ef0ea3813e19b4c9aeaee3ab903a7a
Gisselle Pombar <gisselle.pombar@gmail.com>
2023-01-12 10:10:07 -0500

    added bonus question to loops hw

--- loops-array-methods.js
+++ loops-array-methods.js
@@ -1,3 +1,18 @@
+//Make a square
+function square (x) {
+
+    for (let i = 0; i < x; i++) {
+        let n = '';
+
+        for (let j = 0; j < x; j++) {
+            n = n + '';
+        }
+
+        console.log(n)
+    }
+}
+square(2);
+
 //1 Create a function that makes a pyramid using a loop
 function pyramid (x) {
 
@@ -34,3 +49,28 @@ function fruitList(arr) {
 }
 
 fruitList(favoriteFruits);
+
+/* Bonus: Write a function named assignGrade, that takes one argument, a number score.
+Return a grade for the score, either “A,” “B,” “C,” “D,” or “F.” Invoke that function for a few different scores and log the result to make sure it works. */
+
+function assignGrade(number) {
+    let score = number;
+
+    if (score >= 90) {
+        console.log('A');
+    }
+    if (score >= 80 && score < 90) {
+        console.log('B');
+    }
+    if (score >= 70 && score < 80) {
+        console.log('C');
+    }
+    if (score >= 60 && score < 70) {
+        console.log('D');
+    }
+    if (score < 60) {
+        console.log('F');
+}
+    return score;
+} 
+assignGrade(88);
